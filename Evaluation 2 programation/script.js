8cec5246f60e70f5a5e81c1882a03c5a87b49b2e
inesnessi <43878117+inesnessi@users.noreply.github.com>
2019-01-14 16:59:00 +0100

    Add files via upload

---
+++ Evaluation 2 programation/script.js
@@ -0,0 +1,23 @@
+
+/* c est un echec pour moi j ai abondoné je retien rien, j ai pas comprie je me suis perdu il est deja 16h55 est toute la classe a deja fini*/
+// create a Array, containing the lines
+var grille =tableau();
+
+// create the lines one after the other
+for(var i=0; i<4; i++);
+grille[i] = new Array("subtances","temperaturedEbulitionEn°C","temperaturedEbulitionEn°C");
+for(var i=0; i<4; i++);
+grille [i]= new Array("acideacetique",17,118);
+for(var i=0; i<4; i++);
+   grille[i] = new Array("acidenitrique","-41",86);
+   for(var i=0; i<4; i++);
+    grille[i] = new Array("acidesulfurique",10,290);
+    for(var i=0; i<4; i++);
+ grille[i] = new Array("alcoolethylique","-114",78);
+ for(var i=0; i<4; i++);
+  grille[i] = new Array("aluminium","-41",2450);
+
+function search(tableau,79){
+  return tableau<79;
+}
+var
